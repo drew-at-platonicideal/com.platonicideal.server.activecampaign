0a60ca2ae6f5634733d5945475b0ffc20420ca07
drew-at-titus <57359899+drew-at-titus@users.noreply.github.com>
2020-04-30 10:05:03 -0400

    Cleanup, move from h2 to mysql

--- src/main/java/com/platonicideal/ServerApplication.java
+++ src/main/java/com/platonicideal/ServerApplication.java
@@ -2,7 +2,6 @@ package com.platonicideal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = { "com.platonicideal" })
@@ -11,10 +10,7 @@ public class ServerApplication {
 
     public static void main(String[] args) throws Exception {
         SpringApplication springApplication = new SpringApplication(ServerApplication.class);
-        ConfigurableApplicationContext context = springApplication.run(args);
-
-        CampaignActivityTestService service = context.getBean(CampaignActivityTestService.class);
-        service.doStuff();
+        springApplication.run(args);
     }
 
 }
